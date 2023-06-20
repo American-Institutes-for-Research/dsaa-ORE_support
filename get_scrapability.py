e6d1e56da6a6ebc23adffc3451a6e60008943645
Mike Trinh <mtrinh@air.org>
2023-06-20 14:08:33 -0400

    Update to use reppy

--- get_scrapability.py
+++ get_scrapability.py
@@ -12,9 +12,46 @@ import retry
 import json
 import sys
 import time
+import reppy
+from reppy.robots import Robots
+import multiprocessing
+import time
+import warnings
+warnings.filterwarnings("ignore")
 
-socket.setdefaulttimeout(15) #How many seconds to wait before skipping a website
+socket.setdefaulttimeout(10) #How many seconds to wait before skipping a website
 ssl._create_default_https_context = ssl._create_unverified_context #fixed a bug involving certificates
+
+
+def parallel_can_fetch(url):
+    return can_fetch(url)
+
+
+def can_fetch(url):
+    try:
+        print(url)
+        if url in ['https://www.sandiegocounty.gov/parks/picnic/snapdragon.html', 
+                   'https://www.sdparks.org/content/sdparks/en/park-pages/SantaYsabel.html', 
+                   'https://www.teamusa.org/usa-softball/about/hall-of-fame/illinois-hall-of-fame']:
+            return 'site skipped'
+        robot_url = Robots.robots_url(url)
+        robots = Robots.fetch(robot_url)
+        return robots.allowed(url, '*')
+    except reppy.exceptions.ConnectionException:
+        return 'invalid URL'
+    except reppy.exceptions.SSLException:
+        if "https://" in url:
+            return can_fetch(url.replace("https://", "http://"))
+        else:
+            return 'SSL Error'
+    except (reppy.exceptions.ExcessiveRedirects, reppy.exceptions.BadStatusCode):
+        # Robots.txt file does not exist
+        return True
+    except reppy.exceptions.MalformedUrl:
+        return 'malformed URL'
+    except reppy.exceptions.ContentTooLong:
+        return 'content too long'
+
 
 @retry.retry(ConnectionError, tries=3, delay=1)
 def check_scrapability(url):
@@ -47,70 +84,99 @@ def check_scrapability(url):
     except:
         return False, 1
     
-#Read in the data
-if len(sys.argv) < 2:
-    print("Please provide a filename or path. The file must contain a 'website' column and a 'scrapability' column for which sites can be scraped (T/F).")
-    sys.exit(1)
-file_path = sys.argv[1]
 
-try:
-    if file_path.lower().endswith('.csv'):
-        df = pd.read_csv(file_path)
-    elif file_path.lower().endswith('.xlsx'):
-        df = pd.read_excel(file_path)
-    else:
-        raise ValueError("Unsupported file format, please provide a .csv or .xlsx file: {}".format(file_path))
-except FileNotFoundError:
-    print("File not found: {}".format(file_path))
-    sys.exit(1)
-
-except pd.errors.ParserError:
-    print("Error parsing the file: {}".format(file_path))
-    sys.exit(1)
-
-except:
-    print("An error occurred while reading the file: {}".format(file_path))
-    sys.exit(1)
-
-renamed = [False, False]
-if 'Website' in df.columns:
-    df = df.rename(columns = {'Website' : 'website'})
-    renamed[0] = True
-if 'website' not in df.columns:
-    print("Error: The file must contain a 'website' column.")
-    sys.exit(1)
-if 'Scrapability' in df.columns:
-    df = df.rename(columns = {'Scrapability' : 'scrapability'})
-    renamed[1] = True
-
-urls = df['website'].tolist()
-scrapability = []
-print(f"Number of websites to check: {len(urls)}")
-start_time = time.time()
-print(f"Current progress: 0 / {len(urls)}         Elapsed time: 0 secs")
-#for each website
-for i in range(len(urls)):
-    if i % 50 == 0 and i != 0:
-        print(f"Current progress: {i} / {len(urls)}         Elapsed time: {round(time.time() - start_time, 1)} secs")
-
-    url = urls[i]
-    if pd.isna(url):
-        scrapability.append('site skipped')
-        continue
-    can_scrape, skipped = check_scrapability(url)
-    if not can_scrape:
-        if skipped == 1:
-            scrapability.append('site skipped')
-        elif skipped == 2:
-            scrapability.append('invalid URL')
+def read_file(file_path):
+    try:
+        if file_path.lower().endswith('.csv'):
+            return pd.read_csv(file_path)
+        elif file_path.lower().endswith('.xlsx'):
+            return pd.read_excel(file_path)
         else:
-            scrapability.append(False)
-        continue
-    scrapability.append(True)
-print(f"Complete.         Elapsed time: {round(time.time() - start_time, 1)} secs\n\n")
+            raise ValueError("Unsupported file format, please provide a .csv or .xlsx file: {}".format(file_path))
+    except FileNotFoundError:
+        print("File not found: {}".format(file_path))
+        sys.exit(1)
+    except pd.errors.ParserError:
+        print("Error parsing the file: {}".format(file_path))
+        sys.exit(1)
+    except:
+        print("An error occurred while reading the file: {}".format(file_path))
+        sys.exit(1)
 
-df['scrapability'] = scrapability
 
-df.to_excel('scrapability.xlsx', index=False)
+def fix_df(df):
+    renamed = [False, False]
+    if 'Website' in df.columns:
+        df = df.rename(columns = {'Website' : 'website'})
+        renamed[0] = True
+    if 'website' not in df.columns:
+        print("Error: The file must contain a 'website' column.")
+        sys.exit(1)
+    if 'Scrapability' in df.columns:
+        df = df.rename(columns = {'Scrapability' : 'scrapability'})
+        renamed[1] = True
+    if 'scrapability' not in df.columns:
+        df['scrapability'] = None
+    cleaned_urls = []
+    for url in df['website']:
+        if not pd.isna(url):
+            url = url.strip().split(' ')[0]
+            if not url.startswith('https://') and not url.startswith('http://'):
+                url = 'https://' + url
+        cleaned_urls.append(url)
+    df['website'] = cleaned_urls
+    return df
 
-print("Output file 'scrapability.xlsx' created.")
+
+def get_scrapability(df, output_fp, overwrite=False, use_reppy=True):
+    start_time = time.time()
+    checked = 0
+    pool = multiprocessing.Pool()
+    processes = {}
+    for i in df.index:
+        # if i % 50 == 0:
+        #     print(f"Current progress: {i} / {df.shape[0]}         Elapsed time: {round(time.time() - start_time, 1)} secs")
+        if not overwrite and not pd.isna(df.loc[i, 'scrapability']) and df.loc[i, 'scrapability'] != 'Malformed URL':
+            continue
+        url = df.loc[i, 'website']
+        if pd.isna(url):
+            df.loc[i, 'scrapability'] = 'site skipped'
+            continue
+        if use_reppy:
+            p = pool.apply_async(parallel_can_fetch, args=(url, ))
+            processes[i] = p
+        else:
+            df.loc[i, 'scrapability'] = can_fetch(url)
+            checked += 1
+            if checked % 10 == 0:
+                df.to_excel('scrapability.xlsx', index=False)
+
+    if use_reppy:
+        for i, p in processes.items():
+            try:
+                if i % 50 == 0:
+                    print(f"Current progress: {i} / {df.shape[0]}         Elapsed time: {round(time.time() - start_time, 1)} secs")
+                    df.to_excel(output_fp, index=False)
+                df.loc[i, 'scrapability'] = p.get(10)
+            except multiprocessing.TimeoutError:
+                df.loc[i, 'scrapability'] = 'timed out'
+            
+    print(f"Complete.         Elapsed time: {round(time.time() - start_time, 1)} secs\n\n")
+    df.to_excel(output_fp, index=False)
+        
+
+def main():
+    #Read in the data
+    if len(sys.argv) < 3:
+        print("Please provide an input and output filepath. The input file must contain a 'website' column and a 'scrapability' column for which sites can be scraped (T/F).")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    output_fp = sys.argv[2]
+
+    df = read_file(file_path)
+    df = fix_df(df)
+    get_scrapability(df, output_fp)
+
+
+if __name__ == '__main__':
+    main()
